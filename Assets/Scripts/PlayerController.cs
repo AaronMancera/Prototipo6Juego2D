e4534cef5c448367ab40ac03348abe0ab84e84ec
Aarón <aaronmanceracantador2003@gmail.com>
2023-11-14 18:08:45 +0100

    Movimiento del personaje

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float velocidad;
     private float horizontal;
     private float vertical;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,9 +19,12 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        //Vector2 position = transform.position;
+        Vector2 position = rb.position;
         position.x = position.x+velocidad*horizontal*Time.deltaTime;
         position.y = position.y+velocidad*vertical*Time.deltaTime;
-        transform.position = position;
+        //transform.position = position;
+        rb.MovePosition(position);
+
     }
 }
